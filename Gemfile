967a759ab50ff28e5f9451f0d93b0fc890a6c5de
MichaelAcostaFreelancer <michaelacostafreelancer@gmail.com>
2026-06-23 15:39:39 -0400

    Fix Vercel Ruby deployment config

--- Gemfile
+++ Gemfile
@@ -1,7 +1,5 @@
 source 'https://rubygems.org'
 
-ruby RUBY_VERSION
-
 gem 'sinatra', '~> 4.1'
 gem 'puma', '~> 6.0'
 gem 'rackup', '~> 2.0'
--- api/index.rb
+++ api/index.rb
@@ -1,21 +1,3 @@
-require 'sinatra/base'
-require 'json'
+require_relative '../app'
 
-class App < Sinatra::Base
-  set :public_folder, File.expand_path('../public', __dir__)
-
-  get '/' do
-    content_type 'text/html'
-    File.read(File.expand_path('../index.html', __dir__))
-  end
-
-  get '/health' do
-    content_type :json
-    { status: 'ok', game: 'space-invaders-remastered' }.to_json
-  end
-
-  get '/api/health' do
-    content_type :json
-    { status: 'ok', game: 'space-invaders-remastered' }.to_json
-  end
-end
+run SpaceInvadersApp
